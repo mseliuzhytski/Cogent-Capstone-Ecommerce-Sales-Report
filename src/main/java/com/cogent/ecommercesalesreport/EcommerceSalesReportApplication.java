fc2d250df09b2cf50c06f6d57c5e8792441604eb
Christopher Gioia <christophergioia64@gmail.com>
2024-03-03 12:22:29 -0800

    initial commit of project

---
+++ src/main/java/com/cogent/ecommercesalesreport/EcommerceSalesReportApplication.java
@@ -0,0 +1,13 @@
+package com.cogent.ecommercesalesreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSalesReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSalesReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cogent/ecommercesalesreport/EcommerceSalesReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogent.ecommercesalesreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSalesReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
