5168bd4159544892eaae1aa571409ffc09c9d6c5
Christopher Gioia <christophergioia64@gmail.com>
2024-03-04 11:00:08 -0800

    Now a eureka client

--- src/main/java/com/cogent/ecommercesalesreport/EcommerceSalesReportApplication.java
+++ src/main/java/com/cogent/ecommercesalesreport/EcommerceSalesReportApplication.java
@@ -3,11 +3,13 @@ package com.cogent.ecommercesalesreport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan(
 		basePackages = {"com.cogent.ecommerce.model"}
 )
+@EnableDiscoveryClient
 public class EcommerceSalesReportApplication {
 
 	public static void main(String[] args) {
